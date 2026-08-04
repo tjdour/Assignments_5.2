6aef0d02d3c1160c7eb918dc6ddd7a68a2dce864
tjdour <tongjdour@outlook.com>
2026-08-04 17:57:56 -0400

    Assignment 5.2.3

--- Assignment_5.2.3/Program.cs
+++ Assignment_5.2.3/Program.cs
@@ -1,7 +1,11 @@
 ﻿//Assignment 5.2.3
 //Write a program in C# Sharp to print numbers from n to 1 using recursion.
 
-RecursivePrint(10);
+Console.WriteLine("Enter a number:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+RecursivePrint(num);
+
 void RecursivePrint(int n)
 {
     if (n == 0)
@@ -11,7 +15,5 @@ void RecursivePrint(int n)
 
     Console.Write(n + " ");
     RecursivePrint(n - 1);
-
-    
 
 }
